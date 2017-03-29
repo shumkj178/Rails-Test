edd09365649b604edf112275826d4e35018f7653
shumkj <seansam06@gmail.com>
2017-03-29 13:09:30 +1000

    Added new route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render text: "hello, world!<br>Yo!"
+    render text: "hello, world!"
+  end
+  def bye
+    render text: "goodbye, world!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get '/hello', to: 'application#hello'
+  get '/bye', to: 'application#bye'
 end
